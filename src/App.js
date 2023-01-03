acc4e8662ef08ce3da6f80e209c9e8da289c201d
pPlanello <planello96@gmail.com>
2023-01-03 12:20:23 +0100

    Implement Meals content without logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Meals from './components/Meals/Meals';
 import ToolBar from './components/shared/ToolBar';
 
 function App() {
   return (
     <>
       <ToolBar />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,13 @@
+import MealsSummary from "./MealsSummary";
+import AvailableMeals from "./AvailableMeals";
+
+const Meals = (props) => {
+    return (
+        <>
+        <MealsSummary />
+        <AvailableMeals />
+        </>
+    );
+};
+
+export default Meals;
