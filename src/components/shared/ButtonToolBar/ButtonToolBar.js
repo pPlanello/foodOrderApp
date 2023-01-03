2a300cebc17011e0e926d26133fbd3d684669c79
pPlanello <planello96@gmail.com>
2023-01-03 16:12:10 +0100

    Implement button cart effect with css

--- src/components/shared/ButtonToolBar/ButtonToolBar.js
+++ src/components/shared/ButtonToolBar/ButtonToolBar.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import classes from './ButtonToolBar.module.css';
 import CartIcon from '../../assets/CartIcon';
@@ -6,14 +6,31 @@ import CartContext from '../../context/CartContext';
 
 const ButtonToolBar = (props) => {
 
+    const [buttonIsHighlighed, setButtonIsHighlighed] = useState(false);
+
     const cartContext = useContext(CartContext);
 
     const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
 
+    const buttonClasses = `${classes.button} ${buttonIsHighlighed ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (cartContext.items.length === 0) {
+            return;
+        }
+        setButtonIsHighlighed(true);
+
+        const timer = setTimeout(() => setButtonIsHighlighed(false), 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [cartContext]);
+
     return (
-        <button onClick={props.onClick} className={classes.button}>
+        <button onClick={props.onClick} className={buttonClasses}>
             <span className={classes.icon}><CartIcon /></span>
             <span>Your Cart</span>
             <span className={classes.badge}>{numberOfCartItems}</span>
